68edfdc661bba8a706670ed0d79831b0dcf3aad6
IsaacNicaDev <chamorroeugenio5@gmail.com>
2023-02-27 13:27:49 -0600

    sextable

--- catalogs/models.py
+++ catalogs/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Sex(models.Model):
+    DESCRIPTION = (
+        ("F", "Femenino"),
+        ("M", "Maculino"),
+    )
+    name = models.CharField(max_length=10, null=False)
+    description = models.CharField(max_length=1, choices=DESCRIPTION)
+    
+    def __str__(self):
+        return self.name
