1dfdf09ba2b4538c3c89306d38023a9220a3d081
IsaacNicaDev <chamorroeugenio5@gmail.com>
2023-02-28 17:06:22 -0600

    serializando

--- catalogs/api.py
+++ catalogs/api.py
@@ -1,9 +1,54 @@
 from rest_framework import viewsets, permissions
 
 from .models import Sexo
+from .models import Grado
+from .models import Asignatura
+from .models import Municipio
+from .models import NivelEducativo
+from .models import Modalidad
+from .models import Seccion
+from .models import Turno
+from .models import CentroEducativo
+from .models import TipoIngreso
+from .models import Ciudad
+from .models import Pais
+from .models import Idioma
+from .models import Etnia
+from .models import Discapacidad
+from .models import Religion
+from .models import Departamento
+from .models import DelegacionDepartamental
+from .models import Zona
+from .models import Barrio
 from .serializers import SexoSerializer
+from .serializers import GradoSerializer
+from .serializers import AsignaturaSerializer
+from .serializers import MunicipioSerializer
+from .serializers import NivelEducativoSerializer
+from .serializers import ModalidadSerializer
+from .serializers import SeccionSerializer
+from .serializers import TurnoSerializer
+from .serializers import CentroEducativoSerializer
+from .serializers import TipoIngresoSerializer
+from .serializers import CiudadSerializer
+from .serializers import PaisSerializer
+from .serializers import IdiomaSerializer
+from .serializers import EtniaSerializer
+from .serializers import DiscapacidadSerializer
+from .serializers import ReligionSerializer
+from .serializers import DepartamentoSerializer
+from .serializers import DepartamentoSerializer
+from .serializers import zonaSerializer
+from .serializers import BarrioSerializer
 
 class SexoViewSet(viewsets.ModelViewSet):
     queryset = Sexo.objects.all()
     permission_classes = [permissions.AllowAny]
     serializer_class = SexoSerializer
+
+class GradoViewSet(viewsets.ModelViewSet):
+    queryset = Grado.objects.all()
+    permission_classes = [permissions.AllowAny]
+    serializer_class = GradoSerializer 
+
+
--- catalogs/serializers.py
+++ catalogs/serializers.py
@@ -1,6 +1,26 @@
 from rest_framework import serializers
 
 from .models import Sexo
+from .models import Grado
+from .models import Asignatura
+from .models import Municipio
+from .models import NivelEducativo
+from .models import Modalidad
+from .models import Seccion
+from .models import Turno
+from .models import CentroEducativo
+from .models import TipoIngreso
+from .models import Ciudad
+from .models import Pais
+from .models import Idioma
+from .models import Etnia
+from .models import Discapacidad
+from .models import Religion
+from .models import Departamento
+from .models import DelegacionDepartamental
+from .models import Zona
+from .models import Barrio
+
 
 class SexoSerializer(serializers.ModelSerializer):
     class Meta:
@@ -8,3 +28,116 @@ class SexoSerializer(serializers.ModelSerializer):
         fields = ('id', 'nombre', 'created_at', 'updated_at')
         read_only_fields = ('created_at',)
 
+class GradoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Grado
+        fields = ('id', 'nombre', 'created_at', 'updated_at')
+        read_only_fields = ('created_at',)
+
+class AsignaturaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Asignatura
+        fields = ('id', 'nombre', 'created_at', 'updated_at')
+        read_only_fields = ('created_at',)
+
+class MunicipioSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Municipio
+        fields = ('id', 'nombre', 'created_at', 'updated_at')
+        read_only_fields = ('created_at',)
+
+class NivelEducativoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = NivelEducativo
+        fields = ('id', 'nombre', 'created_at', 'updated_at')
+        read_only_fields = ('created_at',)
+
+class ModalidadSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Modalidad
+        fields = ('id', 'nombre', 'created_at', 'updated_at')
+        read_only_fields = ('created_at',)
+
+class SeccionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Seccion
+        fields = ('id', 'nombre', 'created_at', 'updated_at')
+        read_only_fields = ('created_at',)
+
+class TurnoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Turno
+        fields = ('id', 'nombre', 'created_at', 'updated_at')
+        read_only_fields = ('created_at',) 
+
+class CentroEducativoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CentroEducativo
+        fields = ('id', 'nombre', 'created_at', 'updated_at')
+        read_only_fields = ('created_at',) 
+
+class TipoIngresoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TipoIngreso
+        fields = ('id', 'nombre', 'created_at', 'updated_at')
+        read_only_fields = ('created_at',) 
+
+class CiudadSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ciudad
+        fields = ('id', 'nombre', 'created_at', 'updated_at')
+        read_only_fields = ('created_at',) 
+
+class PaisSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Pais
+        fields = ('id', 'nombre', 'created_at', 'updated_at')
+        read_only_fields = ('created_at',) 
+
+class IdiomaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Idioma
+        fields = ('id', 'nombre', 'created_at', 'updated_at')
+        read_only_fields = ('created_at',) 
+
+class EtniaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Etnia
+        fields = ('id', 'nombre', 'created_at', 'updated_at')
+        read_only_fields = ('created_at',) 
+
+class DiscapacidadSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Discapacidad
+        fields = ('id', 'nombre', 'created_at', 'updated_at')
+        read_only_fields = ('created_at',) 
+
+class ReligionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Religion
+        fields = ('id', 'nombre', 'created_at', 'updated_at')
+        read_only_fields = ('created_at',)
+
+class DepartamentoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Departamento
+        fields = ('id', 'nombre', 'created_at', 'updated_at')
+        read_only_fields = ('created_at',) 
+
+class DelegacionDepartamentalSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DelegacionDepartamental
+        fields = ('id', 'nombre', 'created_at', 'updated_at')
+        read_only_fields = ('created_at',) 
+
+class zonaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Zona
+        fields = ('id', 'nombre', 'created_at', 'updated_at')
+        read_only_fields = ('created_at',)
+
+class BarrioSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Barrio
+        fields = ('id', 'nombre', 'created_at', 'updated_at')
+        read_only_fields = ('created_at',)                                                                                                                              
